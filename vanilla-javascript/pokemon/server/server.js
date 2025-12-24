010b8b779f90abb73e087bb4f0fec3e2c119cd7b
jy2058 <jydev763@gmail.com>
2025-12-24 14:57:46 +0900

    init

---
+++ vanilla-javascript/pokemon/server/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, "..", "index.html")));
+app.get("/*all", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "index.html"));
+});
+app.listen(PORT, () => {
+  console.log("HELLO");
+});
